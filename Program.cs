6ce81f0e968e268068b872fef02b301340703b51
jeevan KC <jeevan.kc@amniltech.com>
2024-02-02 10:41:36 +0545

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+#if NET5_0
+            Console.WriteLine("Running on .NET 5.0");
+#elif NET6_0
+            Console.WriteLine("Running on .NET 6.0");
+#elif NET7_0
+            Console.WriteLine("Running on .NET 7.0");
+#else
+        Console.WriteLine("Running on an unknown version");
+#endif
+        Console.WriteLine("Hello, World!");
+    }
+}
